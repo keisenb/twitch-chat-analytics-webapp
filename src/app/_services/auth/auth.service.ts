f328f8408bfb805ffaa4013f0d63ba2fd2d9ab1c
Kyle Eisenbarger <keisenb@ksu.edu>
2018-11-09 01:09:53 -0600

    Added silent token renewals

--- src/app/_services/auth/auth.service.ts
+++ src/app/_services/auth/auth.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { filter } from 'rxjs/operators';
+import { filter, mergeMap } from 'rxjs/operators';
 import * as auth0 from 'auth0-js';
 import { environment } from 'src/environments/environment';
+import { Observable, of, timer } from 'rxjs';
 
 (window as any).global = window;
 
@@ -20,7 +21,43 @@ export class AuthService {
     audience: 'https://api.analytics.keisenb.io'
   });
 
+  refreshSubscription: any;
+
   constructor(public router: Router) {}
+
+  public scheduleRenewal() {
+    if (!this.isAuthenticated()) { return; }
+    this.unscheduleRenewal();
+
+    const expiresAt = JSON.parse(window.localStorage.getItem('expires_at'));
+
+    const expiresIn$ = of(expiresAt).pipe(
+      mergeMap(
+        expAt => {
+          const now = Date.now();
+          // Use timer to track delay until expiration
+          // to run the refresh at the proper time
+          return timer(Math.max(1, expAt - now));
+        }
+      )
+    );
+
+    // Once the delay time from above is
+    // reached, get a new JWT and schedule
+    // additional refreshes
+    this.refreshSubscription = expiresIn$.subscribe(
+      () => {
+        this.renewToken();
+        this.scheduleRenewal();
+      }
+    );
+  }
+
+  public unscheduleRenewal() {
+    if (this.refreshSubscription) {
+      this.refreshSubscription.unsubscribe();
+    }
+  }
 
   public login(): void {
     this.auth0.authorize();
@@ -45,6 +82,7 @@ export class AuthService {
     localStorage.setItem('access_token', authResult.accessToken);
     localStorage.setItem('id_token', authResult.idToken);
     localStorage.setItem('expires_at', expiresAt);
+    this.scheduleRenewal();
   }
 
   public logout(): void {
@@ -52,6 +90,8 @@ export class AuthService {
     localStorage.removeItem('access_token');
     localStorage.removeItem('id_token');
     localStorage.removeItem('expires_at');
+    this.unscheduleRenewal();
+
     // Go back to the home route
     this.router.navigate(['/']);
   }
@@ -65,5 +105,15 @@ export class AuthService {
 
   public accessToken(): string {
     return localStorage.getItem('access_token');
+  }
+
+  public renewToken() {
+    this.auth0.checkSession({}, (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        this.setSession(result);
+      }
+    });
   }
 }
